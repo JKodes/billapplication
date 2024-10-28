8a0cf0b9011cd2120d7c46f8b863fef4efcc822d
JKodes <adrainford09@gmail.com>
2024-10-28 10:09:57 -0400

    mistake were made but fixed

---
+++ billapp/target/debug/billapp.d
@@ -0,0 +1,1 @@
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/billapp: /Users/jawanistanford/Developer/Bills/billapp/src/main.rs
---
+++ billapp/target/debug/deps/billapp-b11212a5fe86a6df.d
@@ -0,0 +1,5 @@
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/billapp-b11212a5fe86a6df: src/main.rs
+
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/billapp-b11212a5fe86a6df.d: src/main.rs
+
+src/main.rs:
