eaf01c7c01bcb84bcb67e6c5346fad718390e131
JKodes <adrainford09@gmail.com>
2024-10-28 10:00:51 -0400

    create file structure

---
+++ billapp/target/debug/deps/billapp-3e9ff5d10797faf5.d
@@ -0,0 +1,5 @@
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/libbillapp-3e9ff5d10797faf5.rmeta: src/main.rs
+
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/billapp-3e9ff5d10797faf5.d: src/main.rs
+
+src/main.rs:
---
+++ billapp/target/debug/deps/billapp-fede99099657d4c5.d
@@ -0,0 +1,5 @@
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/libbillapp-fede99099657d4c5.rmeta: src/main.rs
+
+/Users/jawanistanford/Developer/Bills/billapp/target/debug/deps/billapp-fede99099657d4c5.d: src/main.rs
+
+src/main.rs:
